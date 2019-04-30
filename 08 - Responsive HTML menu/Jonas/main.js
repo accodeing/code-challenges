d5196e5566a583a38b8dffb69fdcad3e0211777e
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2019-04-30 14:02:25 +0200

    Adds Jonas' contribution

---
+++ 08 - Responsive HTML menu/Jonas/main.js
@@ -0,0 +1,3 @@
+document.querySelector('.main-navigation__mobile-menu-toggle').addEventListener('click', event => {
+  document.querySelector('.main-navigation').classList.toggle('main-navigation--is-active');
+});
