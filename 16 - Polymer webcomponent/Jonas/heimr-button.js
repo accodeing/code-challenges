b7d0a303ba5122c10dad69b0accf678af3f50be5
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2019-08-12 09:29:31 +0200

    Adds Jonas's contribution

---
+++ 16 - Polymer webcomponent/Jonas/heimr-button.js
@@ -0,0 +1,56 @@
+import { LitElement, html } from '@polymer/lit-element';
+
+/**
+ * `heimr-button`
+ * Simple Polymer litElement version of the button code challenge
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class HeimrButton extends LitElement {
+  static get properties() {
+    return { icon: String };
+  }
+
+  render(){
+    /**
+     * `render` must return a lit-html `TemplateResult`.
+     *
+     * To create a `TemplateResult`, tag a JavaScript template literal
+     * with the `html` helper function:
+     */
+    return html`
+    <style>
+      :host {
+        display: block;
+        font-family: sans-serif;
+        text-align: center;
+      }
+
+      button {
+        font-size: 1rem;
+        background-color: #bd8dbd;
+        border-radius: 4px;
+        box-shadow: 0 3px 0 #858, 0 3px 5px #aaa;
+        padding: .25rem .5rem;
+        border: none;
+        border-top: 1px solid #e2b9e2;
+        margin: .5rem;
+      }
+
+      img {
+        max-height: 1rem;
+        display: inline-block;
+        vertical-align: middle;
+        margin-right: .5rem;
+      }
+    </style>
+    ${this.icon?
+      html`<button><img src="${this.icon}" /><slot></slot></button>`:
+      html`<button><slot></slot></button>`}
+    `;
+  }
+}
+
+window.customElements.define('heimr-button', HeimrButton);
