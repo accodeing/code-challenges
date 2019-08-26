f6182a6b70a8b5a897062136798c36c8efc27f1c
Hannes Elvemyr <hannes@accodeing.com>
2019-08-26 09:53:56 +0200

    Challenge nr 18

---
+++ 18 - litElement star struck rating/Hannes/README.md
@@ -0,0 +1,17 @@
+# \<heimr-star-struck-rating\>
+
+litElement version of the star struck rating component. Builds on last weeks starstruck component.
+
+This did not go well. There are a lot of concepts that I need to learn about in litElement. I even had trouble building with rollup... So nothing to see from this weeks challenge.
+
+## Install deps
+
+```
+$ yarn
+```
+
+## Viewing Your Element
+
+```
+$ polymer serve
+```
---
+++ 18 - litElement star struck rating/Hannes/src/heimr-star-struck-rating.js
@@ -0,0 +1,43 @@
+import { LitElement, html } from "lit-element";
+
+class LitElementStarStruckRating extends LitElement {
+  constructor() {
+    super();
+    this.stars = this.getAttribute("stars");
+    this.rating = this.getAttribute("rating") || 0;
+  }
+
+  static get properties() {
+    return {
+      stars: { type: Number },
+      rating: { type: Number },
+    };
+  }
+
+  set rating(val) {
+    this.old_value = this._rating;
+    let new_value = Math.min(this.stars, parseInt(val) || 0);
+    const change = new_value - this.rating;
+
+    if (change === 0 && new_value === 1) {
+      new_value = 0;
+    }
+
+    this._rating = new_value;
+    this.requestUpdate("rating", old_value);
+  }
+
+  set stars(val) {
+    this.old_value = this._stars;
+    this._stars = parseInt(val) || 5;
+    this.rating(Math.min(this._rating, this._stars));
+    this.requestUpdate("stars", old_value);
+  }
+
+  render() {}
+}
+
+window.customElements.define(
+  "lit-heimr-star-struck",
+  LitElementStarStruckRating,
+);
