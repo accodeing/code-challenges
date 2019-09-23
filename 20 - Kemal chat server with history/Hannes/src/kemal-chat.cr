541a742ece6674e1b47ac400c533d41a41a22b1e
Hannes Elvemyr <hannes@accodeing.com>
2019-09-23 08:05:51 +0200

    Adds Hannes' code for challenge 20

---
+++ 20 - Kemal chat server with history/Hannes/src/kemal-chat.cr
@@ -0,0 +1,34 @@
+require "kemal"
+
+logging false
+serve_static false
+
+SOCKETS = [] of HTTP::WebSocket
+
+MESSAGES = [] of String
+
+get "/" do
+  render "views/index.ecr"
+end
+
+ws "/chat" do |socket|
+  # Add the client to SOCKETS list
+  SOCKETS << socket
+
+  # Broadcast each message to all clients
+  socket.on_message do |message|
+    MESSAGES << message
+    SOCKETS.each { |socket| socket.send message}
+  end
+
+  # Remove clients from the list when it's closed
+  socket.on_close do
+    SOCKETS.delete socket
+  end
+end
+
+get "/history" do
+  MESSAGES.to_json
+end
+
+Kemal.run
