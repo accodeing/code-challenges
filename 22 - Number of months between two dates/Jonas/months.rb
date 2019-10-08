fcb6c53c04a66155dcabd72fe646816f49a27a10
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2019-10-08 09:24:41 +0200

    Cleanup

--- 22 - Number of months between two dates/Jonas/months.rb
+++ 22 - Number of months between two dates/Jonas/months.rb
@@ -21,28 +21,16 @@ def end_fraction_of_month( date: )
 end
 
 def months( start_date:, end_date: )
-  start_date.day # 1-31
-
-  start_date.month # 1-12
-  start_date.year # 0-9999
-
   years_differance = end_date.year - start_date.year
   whole_months_differance = months_differance( start_month: start_date.month, end_month: end_date.month )
 
-  start_dates_fraction = start_fraction_of_month( date: start_date )
   if( start_date.month == end_date.month )
     start_dates_fraction = start_fraction_of_month( date: start_date ) - ( 1.0 - end_fraction_of_month( date: end_date ))
     end_dates_fraction = 0.0
   else
+    start_dates_fraction = start_fraction_of_month( date: start_date )
     end_dates_fraction = end_fraction_of_month( date: end_date )
   end
-
-  # puts "---------------------------"
-  # puts "#{start_date} - #{end_date}"
-  # puts "years: #{years_differance}"
-  # puts "months: #{whole_months_differance}"
-  # puts "start_dates_fraction: #{start_dates_fraction}"
-  # puts "end_dates_fraction: #{end_dates_fraction}"
 
   total = ( years_differance * 12 ) + whole_months_differance + start_dates_fraction + end_dates_fraction
 
