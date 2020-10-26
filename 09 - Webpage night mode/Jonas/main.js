422a15af85ec8fe5439b22c023871f0251b6de11
Jonas Schubert Erlandsson <jonas@accodeing.com>
2020-10-26 11:10:40 +0100

    Adds theme detection

--- 09 - Webpage night mode/Jonas/main.js
+++ 09 - Webpage night mode/Jonas/main.js
@@ -1,3 +1,5 @@
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
 document.querySelector('.main-navigation__mobile-menu-toggle').addEventListener('click', event => {
   document.querySelector('.main-navigation').classList.toggle('main-navigation--is-active');
 });
@@ -5,3 +7,9 @@ document.querySelector('.main-navigation__mobile-menu-toggle').addEventListener(
 document.querySelector('.theme-switcher__checkbox').addEventListener('change', event => {
   document.documentElement.classList.toggle('dark-mode');
 });
+
+if( prefersDarkScheme ){
+  console.log('Dark mode');
+  document.querySelector('.theme-switcher__checkbox').checked="true";
+  document.documentElement.classList.add('dark-mode');
+}
