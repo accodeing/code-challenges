c81c42d779c32fee5f47069d5fc57ee57796029b
Jonas Schubert Erlandsson <jonas@accodeing.com>
2023-09-26 10:06:20 +0300

    Week two

--- 39 - HTMX/Jonas/index.js
+++ 39 - HTMX/Jonas/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const fs = require("fs").promises;
 const Koa = require("koa");
 const Router = require("@koa/router");
 const serve = require("koa-static");
@@ -10,12 +11,31 @@ const path = require("path");
 const app = new Koa();
 const router = new Router();
 
-let todos = [];
+async function readTodosFromFile() {
+  try {
+    const data = await fs.readFile("todos.json", "utf8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.error("Error reading todos:", err);
+    return [];
+  }
+}
 
-todos.push({
-  task: "Rake the yard.",
-  done: false,
-});
+async function writeTodosToFile(todos) {
+  try {
+    await fs.writeFile("todos.json", JSON.stringify(todos, null, 2), "utf8");
+  } catch (err) {
+    console.error("Error writing todos:", err);
+  }
+}
+
+let todos;
+
+const order_todos = () => {
+  const done = todos.filter((todo) => todo.done);
+  const ramaining = todos.filter((todo) => !todo.done);
+  todos = [...ramaining, ...done];
+};
 
 render(app, {
   root: path.join(__dirname, "views"),
@@ -33,13 +53,26 @@ router.get("koa-example", "/todos", (ctx) => {
   return ctx.render("todos", { todos });
 });
 
-router.post("koa-example", "/todos", (ctx) => {
+router.post("koa-example", "/todos/:index", async (ctx) => {
+  todos[ctx.params.index].done = !todos[ctx.params.index].done;
+
+  order_todos();
+
+  await writeTodosToFile(todos);
+
+  return ctx.render("todos", { todos });
+});
+
+router.post("koa-example", "/todos", async (ctx) => {
   todos.push({
     ...ctx.request.body,
+    done: false,
   });
-  console.log("----");
-  console.log(todos);
-  console.log("----");
+
+  order_todos();
+
+  await writeTodosToFile(todos);
+
   return ctx.render("todos", { todos });
 });
 
@@ -49,4 +82,12 @@ app.use(serve(path.join(__dirname, "/public")));
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(1234);
+async function main() {
+  todos = await readTodosFromFile();
+
+  console.log("Server starting on http://localhost:1234");
+
+  app.listen(1234);
+}
+
+main();
